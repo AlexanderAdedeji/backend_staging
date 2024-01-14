15983ab985a9d416bb64bf9c88d7fb0dc87f0e75
AlexanderAdedeji <87940570+AlexanderAdedeji@users.noreply.github.com>
2024-01-14 20:19:15 +0000

    Update session.py

--- app/database/session.py
+++ app/database/session.py
@@ -8,16 +8,17 @@ from app.settings.configurations.config import Settings
 
 settings =Settings()
 
-uri = settings.DATABASE_URL
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
+SQLALCHEMY_DATABASE_URL = (
+    #  "sqlite:///./sql_app.db"
+    f"postgresql+psycopg2://"
+    f"identikoPostgresSqlDevAdmin@identiko-postgres-dev-server:"
+    f"hd1KHyW7FFJGkpQ53t31@"
+    f"identiko-postgres-dev-server.postgres.database.azure.com:"
+    f"5432/EaffidavitStaging"
+)
 
-if settings.DEBUG == True:
-    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-else:
-    SQLALCHEMY_DATABASE_URL = uri
 
-    
+
 # SQLALCHEMY_DATABASE_URL = "postgresql://ozswcniofjwjez:70e98db7574cc95162050138b56dbd6e4db6c562072c2a5f4647943280ab4d05@ec2-54-157-16-196.compute-1.amazonaws.com:5432/dc60igb8j3grgv"
 
 
